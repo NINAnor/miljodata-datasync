1eef4d576b18427b564a0c47acc2a93bd2b5a890
Niccolo Cantu <niccolo.cantu@nina.no>
2025-12-22 23:23:46 +0100

    chg: add script to load a parquet with grass metadata to dms

--- src/datasync/main.py
+++ src/datasync/main.py
@@ -4,7 +4,7 @@
 
 import typer
 
-from . import dms, ninagen, nva, pit_registering_salmon, ubw
+from . import dms, grass, ninagen, nva, pit_registering_salmon, ubw
 
 app = typer.Typer(
     help="Provide subcommands for synchronizing different resources, see subcommands"
@@ -14,6 +14,7 @@ app.add_typer(ubw.app, name="ubw")
 app.add_typer(dms.app, name="dms")
 app.add_typer(ninagen.app, name="ninagen")
 app.add_typer(pit_registering_salmon.app, name="pit-registering-salmon")
+app.add_typer(grass.app, name="grass-gis")
 
 if __name__ == "__main__":
     app()
