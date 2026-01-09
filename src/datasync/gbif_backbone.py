575722197a175ca7415bd5d9b41c3a5155faf9aa
Francesco Frassinelli <francesco.frassinelli@nina.no>
2026-01-09 13:13:55 +0100

    chg: use Duckdb Python API for GBIF backbone

--- src/datasync/gbif_backbone.py
+++ src/datasync/gbif_backbone.py
@@ -1,19 +1,15 @@
-from importlib.resources import files
-
-import duckdb
 import fsspec
 import typer
+from duckdb import (
+    DuckDBPyConnection,
+    connect,
+)
 
-from .libs.helpers import pa_read_tsv
+from .libs.helpers import DuckDBAtomicTransaction
 from .settings import (
     env,
     log,
 )
-
-resources = files(__package__).joinpath("gbif_backbone")
-taxon_sql = resources.joinpath("taxon.sql").read_text()
-vernacularname_sql = resources.joinpath("vernacularname.sql").read_text()
-
 
 log.debug("Importing GBIF Backbone settings")
 
@@ -29,23 +25,41 @@ GBIF_BACKBONE_URL = env.url(
 app = typer.Typer(help="export GBIF Backbone data to DuckDB database")
 
 
-def import_taxon(conn, archive):
+def import_taxon(conn: DuckDBPyConnection, archive):
     log.debug("Importing Taxon.tsv")
-    pa_taxon = pa_read_tsv(archive, "Taxon.tsv")  # noqa: F841
-    conn.execute(taxon_sql)
+    conn.sql("DROP TABLE IF EXISTS taxon")
+    conn.from_csv_auto(archive.open("Taxon.tsv")).to_table("taxon")
+    conn.sql("CREATE INDEX taxon_taxonid ON taxon (taxonid)")
+    conn.sql("""
+        PRAGMA create_fts_index(
+            "taxon", "taxonID", "canonicalName", overwrite=TRUE
+        )
+    """)
 
 
-def import_vernacular_name(conn, archive):
+def import_vernacular_name(conn: DuckDBPyConnection, archive):
     log.debug("Importing VernacularName.tsv")
-    pa_vernacular_names = pa_read_tsv(archive, "VernacularName.tsv")  # noqa: F841
-    conn.execute(vernacularname_sql)
+    conn.sql("DROP TABLE IF EXISTS vernacular_name")
+    vernacular_names = conn.from_csv_auto(archive.open("VernacularName.tsv"))  # noqa: F841
+    conn.sql("""
+        SELECT *, CONCAT_WS('|', taxonID, language, vernacularName) AS vernacularID
+        FROM vernacular_names
+    """).to_table("vernacular_name")
+    conn.sql("CREATE INDEX vernacular_name_taxonid ON vernacular_name (taxonid)")
+    conn.sql("CREATE INDEX vernacular_name_language ON vernacular_name (language)")
+    conn.sql("""
+        PRAGMA create_fts_index(
+            "vernacular_name", "vernacularID", "vernacularName", overwrite=TRUE
+        )
+    """)
 
 
 @app.command()
 def import_all():
     """Import GBIF Backbone data into a DuckDB database."""
     archive = fsspec.filesystem("zip", fo=GBIF_BACKBONE_URL.geturl(), mode="r")
-    with duckdb.connect(GBIF_BACKBONE_DUCKDB_NAME) as conn:
-        import_taxon(conn, archive)
-        import_vernacular_name(conn, archive)
+    with connect(GBIF_BACKBONE_DUCKDB_NAME) as conn:
+        with DuckDBAtomicTransaction(conn):
+            import_taxon(conn, archive)
+            import_vernacular_name(conn, archive)
     log.info("GBIF Backbone data imported successfully")
--- src/datasync/gbif_backbone/taxon.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE TABLE taxon AS FROM pa_taxon;
-CREATE INDEX taxon_taxonid ON taxon (taxonid);
-PRAGMA create_fts_index(
-    'taxon', 'taxonID', 'canonicalName',
-    overwrite=TRUE
-);
