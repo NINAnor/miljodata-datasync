50f3de3b3df4dd841c3790fce4e785d247f6d8aa
Taheera Ahmed <taheera.ahmed@nina.no>
2026-01-09 09:45:49 +0100

    fix: update environment variable names for AWS credentials and endpoint

--- src/datasync/nva.py
+++ src/datasync/nva.py
@@ -15,9 +15,10 @@ from .settings import (
 NVA_BASE_URL = env("NVA_BASE_URL", default="https://api.nva.unit.no/")
 NVA_DUCKDB_NAME = env("NVA_DUCKDB_FILE_NAME", default="nva_sync")
 NVA_INSTITUTION_CODE = env("NVA_INSTITUTION_CODE", default="7511.0.0.0")
-NVA_ACCESS_KEY = env("NVA_ACCESS_KEY", default="")
-NVA_SECRET_KEY = env("NVA_SECRET_KEY", default="")
-NVA_ENDPOINT = env("NVA_ENDPOINT", default="https://s3-int-1.nina.no")
+
+NVA_ACCESS_KEY = env("AWS_ACCESS_KEY", default="")
+NVA_SECRET_KEY = env("AWS_SECRET_KEY", default="")
+NVA_ENDPOINT = env("S3_ENDPOINT", default="https://s3-int-1.nina.no")
 NVA_BUCKET = env("NVA_BUCKET", default="nva")
 NVA_PREFIX = env("NVA_PREFIX", default="nva")
 NVA_REGION = env("NVA_REGION", default="us-east-1")
